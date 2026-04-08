01140ba3a290c050afb7832229f263f45133bbc4
dimamarketing001-netizen <dimamarketing001@gmail.com>
2026-04-08 15:37:25 +0500

    RE

--- b24_main_app/static/script.js
+++ b24_main_app/static/script.js
@@ -6,10 +6,9 @@ BX24.ready(() => {
     if (dashboardContainer) {
         // Если найден, просто вставляем в него приветственный текст.
         dashboardContainer.innerHTML = '<h1 style="text-align: center; margin-top: 50px;">Привет</h1>';
-        console.log("Простой скрипт успешно выполнен. Элемент 'dashboard-container' найден и изменен.");
+        console.log("Простой скрипт v1.4 успешно выполнен. Элемент 'dashboard-container' найден и изменен.");
     } else {
         // Если контейнер не найден, выводим критическую ошибку в консоль.
-        // Это поможет нам понять, загружается ли вообще правильный HTML.
-        console.error("Критическая ошибка: Элемент #dashboard-container не найден. Невозможно отобразить приветствие.");
+        console.error("Критическая ошибка v1.4: Элемент #dashboard-container не найден.");
     }
 });
