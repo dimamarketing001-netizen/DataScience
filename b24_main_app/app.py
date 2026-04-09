f649bce30eaf681db962bac6ccacf32c88eb8250
dimamarketing001-netizen <dimamarketing001@gmail.com>
2026-04-09 09:53:37 +0500

    RE

--- b24_main_app/app.py
+++ b24_main_app/app.py
@@ -165,6 +165,6 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():
+    # with app.app_context():
         # init_db() # Create the table on startup
     app.run(debug=True, port=5002)
